eb17e5b5254f506404b5059faacf7ce97a044f32
Chamaththa Shamod <99489115+Chamaththa123@users.noreply.github.com>
2023-09-23 23:07:30 +0530

    init

---
+++ baby-care/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './component/Home';
+import AllVaccine from './component/Vaccine/AllVaccine';
+
+const Stack = createNativeStackNavigator();
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+<Stack.Screen name='Home' component={Home} />
+<Stack.Screen name='AllVaccine' component={AllVaccine} />
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+};
+
+
+export default App;
---
+++ baby-care/component/Home.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, TextInput, Button, ImageBackground } from 'react-native';
+
+const backgroundImage = require('../assets/bg.png');
+const Home = ({ navigation }) => {
+  
+    useEffect(() => {
+        navigation.setOptions({
+          headerShown: false,
+        });
+      }, []);
+  return (
+    <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
+    <View style={styles.container}>
+      <Text style={styles.text}>All Data from Firebase</Text>
+      <Button title='Go to Home Screen' onPress={() => navigation.navigate('AllVaccine')} />
+    </View>
+    </ImageBackground>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
+});
+
+export default Home;
