ae83d99344ede3c33c3df0ad4b4a92465722e9fe
Chamaththa Shamod <99489115+Chamaththa123@users.noreply.github.com>
2023-10-19 11:50:40 +0530

    Update MotherAllClinic.js

--- baby-care/component/Vaccine/Mother/MotherAllClinic.js
+++ baby-care/component/Vaccine/Mother/MotherAllClinic.js
@@ -1,46 +1,31 @@
-import React, { useEffect, useState } from 'react';
-import { Text, View, StyleSheet, ImageBackground, Image, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, ImageBackground } from 'react-native';
 import { TabView, TabBar } from 'react-native-tab-view';
 import ClinicDetails from './MotherClinic';
-import HealthGraphs from './Health Graphs/MotherHealthGraphs';
-
-
-const Tab1Screen = ({ item }) => (
-  <View>
-    <ClinicDetails data={item} />
-  </View>
-);
-
-const Tab2Screen = ({ item }) => (
-  <View>
-    {/* <HealthGraphs data={item}/> */}
-  </View>
-);
+// import HealthGraphs from './Health Graphs/MotherHealthGraphs';
 
 const initialRoutes = [
   { key: 'tab1', title: 'Clinic Details' },
   { key: 'tab2', title: 'Health Graphs' },
 ];
 
-const AllClinic = ({ route, navigation, data }) => {
+const AllClinic = ({ route, data }) => {
   const [index, setIndex] = useState(0);
   const [routes] = useState(initialRoutes);
 
   const renderScene = ({ route }) => {
     switch (route.key) {
       case 'tab1':
-        return <Tab1Screen item={data} />;
+        return <ClinicDetails data={data} />;
       case 'tab2':
-        return <Tab2Screen item={data} />;
+      //   return <HealthGraphs data={data} />;
       default:
         return null;
     }
   };
 
   return (
-
     <View style={styles.container}>
-
       <TabView
         navigationState={{ index, routes }}
         renderScene={renderScene}
@@ -58,7 +43,6 @@ const AllClinic = ({ route, navigation, data }) => {
         style={{ marginTop: 20 }}
       />
     </View>
-
   );
 };
 
@@ -66,50 +50,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     width: '100%',
-  },
-  contentContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginLeft: 30,
-    marginTop: 70,
-  },
-  text: {
-    fontSize: 25,
-    fontWeight: 'bold',
-    marginLeft: 30,
-    marginRight: 90,
-  },
-  backgroundImage: {
-    flex: 1,
-    resizeMode: 'cover',
-    justifyContent: 'center',
-  },
-  buttonContainer: {
-    alignItems: 'flex-end',
-    marginRight: 20,
-    marginTop: 50,
-  },
-  buttonStyle: {
-    backgroundColor: '#5bf6db',
-    padding: 13,
-    borderRadius: 35,
-    width: 150,
-    height: 50,
-  },
-  buttonText: {
-    color: 'black',
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-  imageStyle: {
-    width: 70,
-    height: 70,
-    resizeMode: 'cover',
-  },
-  scene: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   tabBarContainer: {
     backgroundColor: 'white',
