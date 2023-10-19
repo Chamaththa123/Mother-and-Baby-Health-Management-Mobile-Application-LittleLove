b01a00ff82c823961d18ebc26436e2803d3d77b8
Chamaththa Shamod <99489115+Chamaththa123@users.noreply.github.com>
2023-10-19 16:02:27 +0530

    Update AddBabyClinic.js

--- baby-care/component/Vaccine/Baby/AddBabyClinic.js
+++ baby-care/component/Vaccine/Baby/AddBabyClinic.js
@@ -1,26 +1,22 @@
 import React, { useEffect, useState } from 'react';
-import { Text, View, StyleSheet, ImageBackground, Image, TouchableOpacity, ScrollView,TextInput,Button } from 'react-native';
-import { ref, query, orderByChild, equalTo, push, onValue, set } from 'firebase/database';
+import { Text, View, StyleSheet, ImageBackground, TouchableOpacity, ScrollView, TextInput } from 'react-native';
+import { ref, push, set } from 'firebase/database';
 import { db } from '../../../firebase/config';
 
-
-
 const backgroundImage = require('../../../assets/bg.png');
-const localImage = require('../../../assets/mother.png');
 
 const AddBabyClinic = ({ route, navigation }) => {
   const { data } = route.params;
 
-  const [age, setage] = useState(''); // Initialize with an empty string
-  const [weight, setweight] = useState(''); // Initialize with an empty string
-  const [height, setheight] = useState(''); // Initialize with an empty string
-
+  const [age, setAge] = useState('');
+  const [weight, setWeight] = useState('');
+  const [height, setHeight] = useState('');
 
   const addClinic = () => {
-    // Create a new subject entry for the user in Firebase
+    // Create a new clinic entry for the baby in Firebase
     const clinicRef = ref(db, 'Clinic');
 
-    // Generate a new unique key for the subject
+    // Generate a new unique key for the clinic entry
     const newClinicKey = push(clinicRef);
 
     set(newClinicKey, {
@@ -31,61 +27,63 @@ const AddBabyClinic = ({ route, navigation }) => {
     })
       .then(() => {
         alert('Clinic details added successfully');
-        setage('');
-        setweight('');
-        setheight('');
+        setAge('');
+        setWeight('');
+        setHeight('');
       })
       .catch((error) => {
         alert(error.message);
       });
   };
 
-
   useEffect(() => {
     navigation.setOptions({
-      headerShown: false,
+      headerStyle: {
+        backgroundColor: '#5bf6db',
+      },
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
+      headerShown: true,
+      title: data.babyname,
     });
   }, []);
 
   return (
     <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
-      <View style={styles.container}>
-      <View style={styles.row}>
-            <Image source={localImage} style={styles.imageStyle} />
-            <Text style={styles.name}>{data.babyname}</Text>
-          </View>
-
-          <Text style={styles.header}>Enter Clinic Details</Text>
-
-        <TextInput
-          placeholder="Enter Age"
-          value={age}
-          onChangeText={(age) => setage(age)} style={styles.textBoxes}
-          keyboardType="numeric"
-        />
-        <TextInput
-          placeholder="Enter Weight"
-          value={weight}
-          onChangeText={(weight) => setweight(weight)} style={styles.textBoxes}
-          keyboardType="numeric"
-        />
-
-        <TextInput
-          placeholder="Enter height"
-          value={height}
-          onChangeText={(height) => setheight(height)} style={styles.textBoxes}
-          keyboardType="numeric"
-        />
-
-<TouchableOpacity
-          style={styles.buttonStyle}
-          onPress={addClinic}
-        >
-          <Text style={styles.buttonText}>Submit Clinic Details</Text>
-        </TouchableOpacity>
-      </View>
+      <ScrollView contentContainerStyle={styles.scrollViewContent}>
+        <View style={styles.container}>
+          <Text style={styles.header}>Enter Baby Clinic Details</Text>
+          <Text style={styles.inputDetails}>Baby Age (months)</Text>
+          <TextInput
+            placeholder="Enter Baby Age"
+            value={age}
+            onChangeText={setAge}
+            style={styles.textBoxes}
+            keyboardType="numeric"
+          />
+          <Text style={styles.inputDetails}>Baby Weight (Kg)</Text>
+          <TextInput
+            placeholder="Enter Baby Weight"
+            value={weight}
+            onChangeText={setWeight}
+            style={styles.textBoxes}
+            keyboardType="numeric"
+          />
+          <Text style={styles.inputDetails}>Baby Height (cm)</Text>
+          <TextInput
+            placeholder="Enter Baby Height"
+            value={height}
+            onChangeText={setHeight}
+            style={styles.textBoxes}
+            keyboardType="numeric"
+          />
+          <TouchableOpacity style={styles.buttonStyle} onPress={addClinic}>
+            <Text style={styles.buttonText}>Submit Clinic Details</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     </ImageBackground>
-
   );
 };
 
@@ -94,59 +92,13 @@ const styles = StyleSheet.create({
     flex: 1,
     width: '100%',
   },
-  name: {
-    fontSize: 25,
-    fontWeight: 'bold',
-    marginBottom: 10,
-    marginTop: 70,
-    marginLeft: 10,
-    width: 260,
-  },
-  item: {
-    padding: 10,
-    backgroundColor: '#ffffff',
-    borderRadius: 10,
-    margin: 10,
-    borderWidth: 2, // Add border width
-    borderColor: '#5bf6db', // Add border color
-  },
-  backgroundImage: {
-    flex: 1,
-    resizeMode: 'cover',
-    justifyContent: 'center',
-  },
-  imageStyle: {
-    width: 80, // Adjust the width as needed
-    height: 80, // Adjust the height as needed
-    resizeMode: 'cover',
-    marginLeft: 20,
-    marginTop: 60,
-  },
-  row: {
-    flexDirection: 'row',
-    marginBottom: 15,
-  },
-  text1: {
+  inputDetails: {
     fontSize: 17,
-    fontWeight: '900',
-    color: '#323232',
+    marginLeft: '5%',
+    marginTop: '3%',
+    marginBottom: '-3%',
   },
-  buttonStyle: {
-    backgroundColor: '#5bf6db',
-    padding: 13,
-    borderRadius: 5,
-    height: 50,
-    margin: 10,
-  },
-  buttonContainer: {
-    marginBottom: 30,
-  },
-  buttonText: {
-    color: 'black',
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },   textBoxes: {
+  textBoxes: {
     width: '90%',
     fontSize: 16,
     padding: 12,
@@ -154,8 +106,8 @@ const styles = StyleSheet.create({
     borderWidth: 0.5,
     borderRadius: 10,
     margin: 20,
-    marginLeft:20,
-    backgroundColor: 'rgba(255, 255, 255, 0.8)', // Background color for text input
+    marginLeft: 20,
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
   },
   buttonStyle: {
     backgroundColor: '#5bf6db',
@@ -164,23 +116,33 @@ const styles = StyleSheet.create({
     width: '90%',
     height: 50,
     margin: 10,
-    marginLeft:20,
+    marginLeft: 20,
+    marginBottom: 20,
     borderColor: 'gray',
-    alignItems: 'center', // Center text horizontally
-    justifyContent: 'center', // Center text vertically
+    borderWidth: 0.5,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   buttonText: {
     color: 'black',
     fontSize: 17,
     fontWeight: 'bold',
   },
-  header:{
-    fontSize:20,
+  header: {
+    fontSize: 20,
     fontWeight: 'bold',
-    marginTop:100,
-    marginBottom:0,
-    marginLeft:20
-  }
+    marginTop: '10%',
+    marginLeft: '5%',
+    marginBottom: '8%',
+  },
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
+  scrollViewContent: {
+    flexGrow: 1,
+  },
 });
 
 export default AddBabyClinic;
