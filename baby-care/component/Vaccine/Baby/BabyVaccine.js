b5a8d9426d23b3ff6a21953f97c14fb51ec5c6a0
Chamaththa Shamod <99489115+Chamaththa123@users.noreply.github.com>
2023-10-19 15:15:58 +0530

    Update BabyVaccine.js

--- baby-care/component/Vaccine/Baby/BabyVaccine.js
+++ baby-care/component/Vaccine/Baby/BabyVaccine.js
@@ -3,14 +3,16 @@ import { View, Text, TouchableOpacity, FlatList, StyleSheet, ScrollView } from '
 import { useNavigation } from '@react-navigation/native';
 import { ref, query, orderByChild, equalTo, get, onValue } from 'firebase/database';
 import { db } from '../../../firebase/config';
+import ProgressBar from 'react-native-progress/Bar';
 
 const BabyVaccine = ({ data }) => {
   const navigation = useNavigation();
   const [vaccineData, setVaccineData] = useState([]);
+  const [vaccineCount, setVaccineCount] = useState(0);
 
   useEffect(() => {
     // Query the Firebase database to retrieve vaccine data for the specific user
-    const vaccineRef = ref(db, 'BabyVaccine');
+    const vaccineRef = ref(db, 'Vaccine');
     const userVaccineQuery = query(
       vaccineRef,
       orderByChild('babyId'),
@@ -22,9 +24,11 @@ const BabyVaccine = ({ data }) => {
         const vaccineData = snapshot.val();
         const vaccineArray = Object.values(vaccineData);
         setVaccineData(vaccineArray);
+        setVaccineCount(vaccineArray.length);
       } else {
         // If no vaccine data found for the user, set an empty array
         setVaccineData([]);
+        setVaccineCount(0);
       }
     });
 
@@ -32,36 +36,74 @@ const BabyVaccine = ({ data }) => {
     return () => unsubscribe();
   }, [data.id]);
 
+  const totalVaccineDoses = 20;
+  const vaccinePercentage = (vaccineCount / totalVaccineDoses) * 100;
+
   return (
     <ScrollView>
       <View style={styles.container}>
         <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.buttonStyle}
-            onPress={() => {
-              navigation.navigate('AddBabyVaccine', { data });
-            }}
-          >
+          <TouchableOpacity style={styles.buttonStyle} onPress={() => navigation.navigate('AddBabyVaccine', { data })}>
             <Text style={styles.buttonText}>Add Details</Text>
           </TouchableOpacity>
         </View>
 
-         {vaccineData.map((item, index) => (
-          <View style={styles.card}>
-          <View style={styles.row}>
-            <View style={styles.leftColumn}>
-              <Text style={styles.cardText}><Text style={styles.cardHeader}>Age:</Text> {item.age}</Text>
-              <Text style={styles.cardText}><Text style={styles.cardHeader}>Date:</Text> {item.date}</Text>
-            </View>
-            <View style={styles.rightColumn}>
-              <Text style={styles.cardText2}><Text style={styles.cardHeader}>Vaccine Type:</Text> {item.type}</Text>
-              <Text style={styles.cardText2}><Text style={styles.cardHeader}>Batch:</Text> {item.batch}</Text>
-            </View>
+        <View style={styles.progressBarContainer}>
+          {/* Progress bar and related information */}
+          <View style={styles.progressBarText}>
+            <View style={styles.redDot} />
+            <Text style={styles.vaccineCountText}>
+              {vaccinePercentage}% of the baby's vaccination process is complete.
+            </Text>
+          </View>
+          <ProgressBar
+            progress={vaccinePercentage / 100}
+            width={null}
+            height={15}
+            color="#56F113"
+            style={{ marginBottom: 20, borderRadius: 30 }}
+          />
+
+          {/* Number of vaccine information */}
+          <View style={styles.progressBarText}>
+            <View style={styles.redDot} />
+            <Text style={styles.vaccineCountText}>
+              Number of vaccine to be given: {totalVaccineDoses}
+            </Text>
+          </View>
+
+          {/* Number of injections received information */}
+          <View style={styles.progressBarText}>
+            <View style={styles.redDot} />
+            <Text style={styles.vaccineCountText}>
+              Number of vaccine received: {vaccineCount}
+            </Text>
           </View>
         </View>
+
+        {vaccineData.map((item, index) => (
+          <View style={styles.card} key={index}>
+            <Text style={styles.cardText}>
+              <Text style={styles.cardHeader}>Mother Gestational Duration (Weeks):</Text> {item.age} Week
+            </Text>
+            <Text style={styles.cardText2}>
+              <Text style={styles.cardHeader}>Type of Vaccine:</Text> {item.type}
+            </Text>
+            <View style={styles.row}>
+              <View style={styles.leftColumn}>
+                <Text style={styles.cardText}>
+                  <Text style={styles.cardHeader}>Date:</Text> {item.date}
+                </Text>
+              </View>
+              <View style={styles.rightColumn}>
+                <Text style={styles.cardText2}>
+                  <Text style={styles.cardHeader}>Batch No:</Text> {item.batch}
+                </Text>
+              </View>
+            </View>
+          </View>
         ))}
-        {vaccineData.length === 0 && (
-          <Text>No vaccine data available for this user.</Text>
-        )} 
+        {vaccineData.length === 0 && <Text>No vaccine data available for this user.</Text>}
       </View>
     </ScrollView>
   );
@@ -71,7 +113,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginLeft: 10,
-    marginRight: 10
+    marginRight: 10,
   },
   card: {
     backgroundColor: 'white',
@@ -98,22 +140,22 @@ const styles = StyleSheet.create({
   },
   rightColumn: {
     flex: 1,
-  },cardHeader:{
-fontWeight:'bold'
+  },
+  cardHeader: {
+    fontWeight: 'bold',
   },
   buttonStyle: {
     backgroundColor: '#5bf6db',
-    padding: 13,
+    padding: 15,
     borderRadius: 10,
     height: 50,
     width: 130,
     borderColor: 'gray',
     borderWidth: 0.5,
-
   },
   buttonContainer: {
+    marginTop: 10,
     marginBottom: 10,
-    marginTop:10
   },
   buttonText: {
     color: 'black',
@@ -123,8 +165,30 @@ fontWeight:'bold'
   },
   row: {
     flexDirection: 'row',
-    justifyContent: 'space-between', // Aligns items horizontally with space between them
+    justifyContent: 'space-between',
+  },
+  progressBarContainer: {
+    marginTop: 20,
+  },
+  progressBarText: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  redDot: {
+    width: 10,
+    height: 10,
+    backgroundColor: '#56F113',
+    borderRadius: 5,
+    marginRight: 10,
+    marginTop: -20,
+  },
+  vaccineCountText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    width: '90%',
+    marginBottom: 20,
   },
 });
+
 
 export default BabyVaccine;
