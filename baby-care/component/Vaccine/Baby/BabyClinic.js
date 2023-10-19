5a1cab480a27a7c5e56291bd60c21acf63092f44
Chamaththa Shamod <99489115+Chamaththa123@users.noreply.github.com>
2023-10-19 16:56:19 +0530

    Update BabyClinic.js

--- baby-care/component/Vaccine/Baby/BabyClinic.js
+++ baby-care/component/Vaccine/Baby/BabyClinic.js
@@ -6,60 +6,45 @@ import { db } from '../../../firebase/config';
 
 const ClinicDetails = ({ data }) => {
   const navigation = useNavigation();
+  const [clinicData, setClinicData] = useState([]);
 
-  // const getBPStatus = (bp) => {
-  //   if (bp < 90) {
-  //     return "Low Blood Pressure";
-  //   } else if (bp >= 90 && bp < 120) {
-  //     return "Normal Blood Pressure";
-  //   } else if (bp >= 120 && bp < 130) {
-  //     return "Elevated Blood Pressure";
-  //   } else if (bp >= 130 && bp < 140) {
-  //     return "High Blood Pressure (Stage 1)";
-  //   } else {
-  //     return "High Blood Pressure (Stage 2)";
-  //   }
-  // };
+  useEffect(() => {
+    const clinicRef = ref(db, 'Clinic');
+    const userClinicQuery = query(
+      clinicRef,
+      orderByChild('babyId'),
+      equalTo(data.id)
+    );
 
-  // const getBPStatusStyle = (bp) => {
-  //   const status = getBPStatus(bp);
-  //   switch (status) {
-  //     case "Low Blood Pressure":
-  //       return { backgroundColor: '#FFA67F', fontWeight: "bold" };
-  //     case "Normal Blood Pressure":
-  //       return { backgroundColor: '#2BBF00', fontWeight: "bold" };
-  //     case "Elevated Blood Pressure":
-  //       return { backgroundColor: '#CFFF1B', fontWeight: "bold" };
-  //     case "High Blood Pressure (Stage 1)":
-  //       return { backgroundColor: '#FF5500', fontWeight: "bold" };
-  //     case "High Blood Pressure (Stage 2)":
-  //       return { backgroundColor: '#D30303', fontWeight: "bold" };
-  //     default:
-  //       return {};
-  //   }
-  // };
+    const unsubscribe = onValue(userClinicQuery, (snapshot) => {
+      if (snapshot.exists()) {
+        const clinicData = snapshot.val();
+        const clinicArray = Object.values(clinicData);
+        clinicArray.reverse();
+        setClinicData(clinicArray);
+      } else {
+        setClinicData([]);
+      }
+    });
+    return () => unsubscribe();
+  }, [data.id]);
 
+  // Calculate BMI function
+  const calculateBMI = (weight, height) => {
+    const heightInMeters = height / 100;
+    return (weight / (heightInMeters * heightInMeters)).toFixed(2);
+  };
 
-  // useEffect(() => {
-  //   const clinicRef = ref(db, 'Clinic');
-  //   const userClinicQuery = query(
-  //     clinicRef,
-  //     orderByChild('motherId'),
-  //     equalTo(data.id)
-  //   );
-
-  //   const unsubscribe = onValue(userClinicQuery, (snapshot) => {
-  //     if (snapshot.exists()) {
-  //       const clinicData = snapshot.val();
-  //       const clinicArray = Object.values(clinicData);
-  //       clinicArray.reverse();
-  //       setClinicData(clinicArray);
-  //     } else {
-  //       setClinicData([]);
-  //     }
-  //   });
-  //   return () => unsubscribe();
-  // }, [data.id]);
+  // Determine BMI status and set text color
+  const getBMIStatus = (bmi) => {
+    if (bmi < 18.5) {
+      return { status: 'Under Nutrition', color: 'red', backgroundColor: "#FF3F3F" };
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+      return { status: 'Proper Nutrition', color: 'green', backgroundColor: "#1DFE24" };
+    } else {
+      return { status: 'Over Nutrition', color: 'orange', backgroundColor: "orange" };
+    }
+  };
 
   return (
     <ScrollView>
@@ -75,31 +60,35 @@ const ClinicDetails = ({ data }) => {
           </TouchableOpacity>
         </View>
 
-        {/* {clinicData.map((item, index) => (
-          <View style={styles.card} key={index}>
-            <Text style={styles.cardText}>
-              <Text style={styles.cardHeader}>Mother Gestational Duration  :</Text> {item.week} Week
-            </Text><Text style={styles.cardText}>
-              <Text style={styles.cardHeader}>Maternal Weight:</Text> {item.weight} Kg
-            </Text>
-            <Text style={styles.cardText}>
-              <Text style={styles.cardHeader}>Fundal Height:</Text> {item.F_height} cm
-            </Text>
-            <Text style={styles.cardText}>
-              <Text style={styles.cardHeader}>Blood Pressure:</Text> {item.bp} mm Hg
-            </Text>
-
-            <View style={[styles.card2, getBPStatusStyle(item.bp)]} key={index}>
-              <Text style={[styles.cardStatus, getBPStatusStyle(item.bp)]}>
-                {getBPStatus(item.bp)}
+        {clinicData.map((item, index) => {
+          const bmiStatus = getBMIStatus(calculateBMI(item.weight, item.height));
+          return (
+            <View style={styles.card} key={index}>
+              <Text style={styles.cardText}>
+                <Text style={styles.cardHeader}>Baby Age (months) :</Text> {item.age} Months
               </Text>
-            </View>
+              <Text style={styles.cardText}>
+                <Text style={styles.cardHeader}>Baby Weight (Kg) :</Text> {item.weight} Kg
+              </Text>
+              <Text style={styles.cardText}>
+                <Text style={styles.cardHeader}>Baby Height (cm) :</Text> {item.height} cm
+              </Text>
+              <Text style={[styles.cardText, { color: bmiStatus.color }]}>
+                <Text style={styles.cardHeader}>Baby BMI Rate :</Text> {calculateBMI(item.weight, item.height)} kg/m^2
+              </Text>
 
-          </View>
-        ))}
+              <View style={[styles.card2, { backgroundColor: bmiStatus.backgroundColor }]}>
+                <Text style={[styles.cardStatus, { color: bmiStatus.color }]}>
+                  <Text style={{ color: 'black', fontWeight: 'bold' }}>{bmiStatus.status}</Text>
+                </Text>
+              </View>
+
+            </View>
+          );
+        })}
         {clinicData.length === 0 && (
           <Text>No clinic data available for this user.</Text>
-        )} */}
+        )}
       </View>
     </ScrollView>
   );
@@ -143,7 +132,8 @@ const styles = StyleSheet.create({
   },
   cardStatus: {
     fontSize: 16,
-    textAlign: 'center'
+    textAlign: 'center',
+    color: 'black'
   },
   cardHeader: {
     fontWeight: 'bold',
