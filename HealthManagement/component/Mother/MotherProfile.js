a23b36cc4f0e9212fbcd198be33d9d8d9bb9a7f7
Chamaththa Shamod <99489115+Chamaththa123@users.noreply.github.com>
2023-11-29 17:10:32 +0530

    Create MotherProfile.js

---
+++ HealthManagement/component/Mother/MotherProfile.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const MotherProfile = () => {
+  return (
+    <View>
+      <Text>MotherProfile</Text>
+    </View>
+  )
+}
+
+export default MotherProfile
