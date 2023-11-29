18a4c7f0469e44e5bf1e9bfe3ed8b80f4acb7917
Chamaththa Shamod <99489115+Chamaththa123@users.noreply.github.com>
2023-11-30 03:28:03 +0530

    Update MotherProfile.js

--- HealthManagement/component/Mother/MotherProfile.js
+++ HealthManagement/component/Mother/MotherProfile.js
@@ -1,7 +1,24 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+
 
 const MotherProfile = () => {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerTitleStyle: {
+        fontWeight: "bold",
+        color: "#57ADF8",
+        fontSize: 20,
+      },
+      headerTintColor: "#57ADF8",
+      headerShown: true,
+      title: "My Profile",
+      headerTitleAlign: "center",
+    });
+  }, [navigation]);
   return (
     <View>
       <Text>MotherProfile</Text>
