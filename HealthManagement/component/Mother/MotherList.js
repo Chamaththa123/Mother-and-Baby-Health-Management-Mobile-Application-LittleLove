3e36822de51a4a2c10624aecef3577363f441b68
Chamaththa Shamod <99489115+Chamaththa123@users.noreply.github.com>
2023-11-30 03:01:05 +0530

    Update MotherList.js

--- HealthManagement/component/Mother/MotherList.js
+++ HealthManagement/component/Mother/MotherList.js
@@ -4,31 +4,32 @@ import {
   Text,
   TouchableOpacity,
   ScrollView,
+  TextInput,
   StyleSheet,
 } from "react-native";
 import { firebase } from "../../firebase/config";
 import { useNavigation } from "@react-navigation/native";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 const MotherList = () => {
+  const navigation = useNavigation();
 
-    const navigation = useNavigation();
+  useEffect(() => {
+    navigation.setOptions({
+      headerTitleStyle: {
+        fontWeight: "bold",
+        color: "#57ADF8",
+        fontSize: 20,
+      },
+      headerTintColor: "#57ADF8",
+      headerShown: true,
+      title: "All Mothers List",
+      headerTitleAlign: "center",
+    });
+  }, [navigation]);
 
-    useEffect(() => {
-        navigation.setOptions({
-          headerTitleStyle: {
-            fontWeight: "bold",
-            color: "#57ADF8",
-            fontSize: 20,
-          },
-          headerTintColor: "#57ADF8",
-          headerShown: true,
-          title: "All Mothers List",
-          headerTitleAlign: 'center',
-        });
-      }, [navigation]);
-      
-  
   const [mothersList, setMothersList] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
 
   const getMothersList = async () => {
     try {
@@ -53,10 +54,26 @@ const MotherList = () => {
     getMothersList();
   }, []);
 
+  const filterMothersByRegisterNo = () => {
+    return mothersList.filter((mother) =>
+      mother.register_No.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+  };
+
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+  };
+
   const renderMothers = () => {
-    return mothersList.map((mother) => (
+    const filteredMothers = filterMothersByRegisterNo();
+    return filteredMothers.map((mother) => (
       <TouchableOpacity key={mother.id} style={styles.motherItem}>
-        <Text style={styles.motherName}>{mother.name}</Text>
+        <View style={{ flexDirection: "row" }}>
+          <View style={styles.card}>
+            <Text>{mother.register_No}</Text>
+          </View>
+          <Text style={styles.motherName}>{mother.name}</Text>
+        </View>
         {/* Display additional details here if needed */}
       </TouchableOpacity>
     ));
@@ -64,7 +81,23 @@ const MotherList = () => {
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
-      
+      <TextInput
+        style={styles.searchInput}
+        placeholder="Search by Registered No."
+        onChangeText={handleSearch}
+        value={searchQuery}
+      />
+      <Icon
+        name="search"
+        size={30}
+        color="#57ADF8"
+        style={{
+          alignSelf: "flex-end",
+          marginRight: "7%",
+          marginTop: -75,
+          marginBottom: 20,
+        }}
+      />
       <ScrollView style={styles.scrollView}>{renderMothers()}</ScrollView>
     </ScrollView>
   );
@@ -88,18 +121,36 @@ const styles = StyleSheet.create({
     marginTop: 10,
     marginBottom: 20,
   },
+  card: {
+    backgroundColor: "#FF25A9",
+    padding: 5,
+    paddingLeft: 10,
+    borderRadius: 10,
+    width: 70,
+    alignSelf: "center",
+  },
   motherItem: {
     marginTop: 20,
     padding: 15,
     borderRadius: 10,
     width: "100%",
-    alignSelf:'center',
-    borderWidth:1,
-    borderColor:'#FF25A9'
+    alignSelf: "center",
+    borderWidth: 1,
+    borderColor: "#FF25A9",
   },
   motherName: {
-    fontSize: 16,
+    fontSize: 18,
     fontWeight: "500",
+    marginLeft: 20,
+  },
+  searchInput: {
+    width: "95%",
+    borderWidth: 1,
+    borderColor: "#57ADF8",
+    borderRadius: 38,
+    padding: 10,
+    marginTop: 20,
+    marginBottom: 35,
   },
 });
 
