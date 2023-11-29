672ed4061dbb4afa0e5f841080b15c845270754c
Chamaththa Shamod <99489115+Chamaththa123@users.noreply.github.com>
2023-11-30 02:31:03 +0530

    Update MotherList.js

--- HealthManagement/component/Mother/MotherList.js
+++ HealthManagement/component/Mother/MotherList.js
@@ -1,12 +1,106 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  StyleSheet,
+} from "react-native";
+import { firebase } from "../../firebase/config";
+import { useNavigation } from "@react-navigation/native";
 
 const MotherList = () => {
-  return (
-    <View>
-      <Text>MotherList</Text>
-    </View>
-  )
-}
 
-export default MotherList
+    const navigation = useNavigation();
+
+    useEffect(() => {
+        navigation.setOptions({
+          headerTitleStyle: {
+            fontWeight: "bold",
+            color: "#57ADF8",
+            fontSize: 20,
+          },
+          headerTintColor: "#57ADF8",
+          headerShown: true,
+          title: "All Mothers List",
+          headerTitleAlign: 'center',
+        });
+      }, [navigation]);
+      
+  
+  const [mothersList, setMothersList] = useState([]);
+
+  const getMothersList = async () => {
+    try {
+      const snapshot = await firebase
+        .firestore()
+        .collection("users")
+        .where("role", "==", 0) // Assuming role 0 identifies mothers
+        .get();
+
+      const mothers = [];
+      snapshot.forEach((doc) => {
+        mothers.push({ id: doc.id, ...doc.data() });
+      });
+
+      setMothersList(mothers);
+    } catch (error) {
+      console.log("Error getting mothers list:", error);
+    }
+  };
+
+  useEffect(() => {
+    getMothersList();
+  }, []);
+
+  const renderMothers = () => {
+    return mothersList.map((mother) => (
+      <TouchableOpacity key={mother.id} style={styles.motherItem}>
+        <Text style={styles.motherName}>{mother.name}</Text>
+        {/* Display additional details here if needed */}
+      </TouchableOpacity>
+    ));
+  };
+
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      
+      <ScrollView style={styles.scrollView}>{renderMothers()}</ScrollView>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+  heading: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginTop: 20,
+    marginBottom: 10,
+  },
+  scrollView: {
+    width: "95%",
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  motherItem: {
+    marginTop: 20,
+    padding: 15,
+    borderRadius: 10,
+    width: "100%",
+    alignSelf:'center',
+    borderWidth:1,
+    borderColor:'#FF25A9'
+  },
+  motherName: {
+    fontSize: 16,
+    fontWeight: "500",
+  },
+});
+
+export default MotherList;
