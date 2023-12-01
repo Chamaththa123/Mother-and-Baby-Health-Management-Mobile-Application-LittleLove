4064d6686643e0d3083a0bbe9581dad8b35b0ab9
Chamaththa Shamod <99489115+Chamaththa123@users.noreply.github.com>
2023-12-01 12:21:09 +0530

    Update MidWifeProfile.js

--- HealthManagement/component/Midwife/MidWifeProfile.js
+++ HealthManagement/component/Midwife/MidWifeProfile.js
@@ -3,35 +3,67 @@ import { View, Text, TouchableOpacity, TextInput, StyleSheet, FlatList } from 'r
 import { firebase } from '../../firebase/config';
 import { useNavigation } from '@react-navigation/native';
 
-const MidWifeProfile = () => {
-  const handleSignOut = () => {
-    console.log('Signing out...');
-    firebase
-      .auth()
-      .signOut()
-      .then(() => {
-        console.log('Sign-out successful');
-        navigation.navigate('Login');
-      })
-      .catch((error) => {
-        console.error('Sign-out error:', error);
-      });
-  };
+const MidWifeProfile = ({ navigation }) => {
+
+  const [name, setName] = useState({});
 
   useEffect(() => {
     const userUid = firebase.auth().currentUser.uid;
-    const userDocRef = firebase.firestore().collection('users').doc(userUid);
+    const userDocRef = firebase.firestore().collection("users").doc(userUid);
 
     userDocRef.get().then((snapshot) => {
       if (snapshot.exists) {
         setName(snapshot.data());
-        loadUserSubjects();
-        loadAllUsers();
       } else {
-        console.log('User doesn\'t exist');
+        console.log("User doesn't exist");
       }
     });
   }, []);
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerTitleStyle: {
+        fontWeight: "bold",
+        color: "#57ADF8",
+        fontSize: 20,
+      },
+      headerTintColor: "#57ADF8",
+      headerShown: true,
+      title: "My Profile",
+      headerTitleAlign: "center",
+    });
+  }, [navigation]);
+
+  const handleSignOut = () => {
+    Alert.alert(
+      "Confirm Sign Out",
+      "Are you sure you want to sign out?",
+      [
+        {
+          text: "Cancel",
+          style: "cancel",
+        },
+        {
+          text: "Sign Out",
+          onPress: () => {
+            console.log("Signing out...");
+            firebase
+              .auth()
+              .signOut()
+              .then(() => {
+                console.log("Sign-out successful");
+                navigation.navigate("Start");
+              })
+              .catch((error) => {
+                console.error("Sign-out error:", error);
+              });
+          },
+        },
+      ],
+      { cancelable: false }
+    );
+  };
+  
 
   return (
     <View>
