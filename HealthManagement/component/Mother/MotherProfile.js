416d660e852b26ef5aac3294189cc454bc8f98fa
Chamaththa Shamod <99489115+Chamaththa123@users.noreply.github.com>
2023-12-01 08:56:28 +0530

    personal details

--- HealthManagement/component/Mother/MotherProfile.js
+++ HealthManagement/component/Mother/MotherProfile.js
@@ -8,8 +8,11 @@ import {
   TouchableOpacity,
   Image,
   ScrollView,
+  Alert,
 } from "react-native";
+import Accordion from "react-native-collapsible/Accordion";
 import { firebase } from "../../firebase/config";
+import PersonalDetails from "./PersonalDetails";
 
 const headerImage = require("../../assets/logo.png");
 const Mother = require("../../assets/mother.png");
@@ -44,9 +47,51 @@ const MotherProfile = ({ navigation }) => {
     });
   }, [navigation]);
 
-  const handleImagePress = () => {
-    navigation.navigate("MotherProfile");
+  const handleSignOut = () => {
+    Alert.alert(
+      "Confirm Sign Out",
+      "Are you sure you want to sign out?",
+      [
+        {
+          text: "Cancel",
+          style: "cancel",
+        },
+        {
+          text: "Sign Out",
+          onPress: () => {
+            console.log("Signing out...");
+            firebase
+              .auth()
+              .signOut()
+              .then(() => {
+                console.log("Sign-out successful");
+                navigation.navigate("Start");
+              })
+              .catch((error) => {
+                console.error("Sign-out error:", error);
+              });
+          },
+        },
+      ],
+      { cancelable: false }
+    );
   };
+
+  const [activeSections1, setActiveSections1] = useState([]);
+
+  const SECTIONS1 = [
+    {
+      title: "Personal Details",
+    },
+  ];
+
+  const renderHeader1 = (section, _, isActive) => (
+    <View style={styles.header_acc}>
+      <Text style={styles.headerText_acc}>{section.title}</Text>
+    </View>
+  );
+
+  const renderContent1 = (section) => <PersonalDetails />;
 
   return (
     <ScrollView style={styles.container}>
@@ -56,6 +101,21 @@ const MotherProfile = ({ navigation }) => {
           <Image source={Mother} style={styles.midwifeImage} />
         </View>
         <Text style={styles.headerName}>{name.email}</Text>
+        <TouchableOpacity
+        style={styles.buttonStyle}
+        onPress={handleSignOut}
+      >
+        <Text style={styles.buttonText}>Sign Out</Text>
+      </TouchableOpacity>
+      </View>
+      <View style={styles.accordion}>
+        <Accordion
+          sections={SECTIONS1}
+          activeSections={activeSections1}
+          renderHeader={renderHeader1}
+          renderContent={renderContent1}
+          onChange={setActiveSections1}
+        />
       </View>
     </ScrollView>
   );
@@ -76,9 +136,17 @@ const styles = StyleSheet.create({
     width: 70,
     height: 70,
   },
+  header_acc: {
+    backgroundColor: "#fff",
+    padding: 10,
+  },
+  headerText_acc: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
   midwifeImage: {
-    width: 50,
-    height: 50,
+    width: 70,
+    height:70,
   },
   headerText: {
     fontSize: 15,
@@ -87,7 +155,7 @@ const styles = StyleSheet.create({
   },
   headerWelcome: {
     fontSize: 19,
-    // color: "#FF25A9",
+    color: "#403A3A",
     fontWeight: "bold",
   },
   text1: {
@@ -99,8 +167,8 @@ const styles = StyleSheet.create({
   headerName: {
     fontSize: 14,
     fontWeight: "bold",
-    // color: "#FF25A9",
-    marginTop: "-4.9%",
+    color: "#403A3A",
+    marginTop:-40,
   },
   card: {
     backgroundColor: "#fff",
@@ -111,43 +179,36 @@ const styles = StyleSheet.create({
     margin: 10,
     borderWidth: 1,
     borderColor: "#FF25A9",
+    marginBottom:30
   },
-  menucard: {
-    backgroundColor: "white",
+  accordion: {
     marginTop: 20,
-    padding: 10,
-    borderRadius: 10,
-    width: 100,
+    margin: 10,
     borderWidth: 1,
-    borderColor: "#57ADF8",
-    height: 100,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  cardText: {
-    fontSize: 13,
-    fontWeight: "bold",
-    color: "#616161",
-  },
-  text: {
-    fontSize: 18,
-    fontWeight: "bold",
-    marginBottom: 10,
-  },
-  backgroundImage: {
-    flex: 1,
-    resizeMode: "cover",
-    justifyContent: "center",
+    borderColor: "#FF25A9",
+    borderRadius: 10,
+    padding: 10,
   },
   row: {
     flexDirection: "row",
     justifyContent: "space-between",
   },
-  row2: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    marginLeft: 20,
-    marginRight: 20,
+  buttonStyle: {
+    backgroundColor: "#FF25A9",
+    padding: 13,
+    borderRadius: 10,
+    width: 100,
+    height: 50,
+    marginTop: 20,
+    borderColor: "#FF25A9",
+    borderWidth: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 17,
+    fontWeight: "bold",
   },
 });
 
