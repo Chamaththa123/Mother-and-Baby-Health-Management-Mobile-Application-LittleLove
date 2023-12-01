8559891428705f870a3eb7fc1a19b1ac7fcd0d02
Chamaththa Shamod <99489115+Chamaththa123@users.noreply.github.com>
2023-12-01 21:16:39 +0530

    Create AddPregnancy.js

---
+++ HealthManagement/component/Mother/AddPregnancy.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const AddPregnancy = () => {
+  return (
+    <View>
+      <Text>AddPregnancy</Text>
+    </View>
+  )
+}
+
+export default AddPregnancy
