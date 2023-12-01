333e516c9d5225e2e37d24e9b8895ca7a7b232fb
Chamaththa Shamod <99489115+Chamaththa123@users.noreply.github.com>
2023-12-01 10:04:28 +0530

    Update Qr.js

--- HealthManagement/component/Mother/Qr.js
+++ HealthManagement/component/Mother/Qr.js
@@ -1,10 +1,25 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { firebase } from "../../firebase/config";
 
 const Qr = () => {
+    const [name, setName] = useState({});
+
+  useEffect(() => {
+    const userUid = firebase.auth().currentUser.uid;
+    const userDocRef = firebase.firestore().collection("users").doc(userUid);
+
+    userDocRef.get().then((snapshot) => {
+      if (snapshot.exists) {
+        setName(snapshot.data());
+      } else {
+        console.log("User doesn't exist");
+      }
+    });
+  }, []);
   return (
     <View>
-      <Text>Qr</Text>
+      <Text>{name.register_No}</Text>
     </View>
   )
 }
