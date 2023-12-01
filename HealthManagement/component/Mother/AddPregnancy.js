45c6182a87f3a0cb226ea37ce77c6e8b4658c4fd
Chamaththa Shamod <99489115+Chamaththa123@users.noreply.github.com>
2023-12-02 02:10:30 +0530

    Update AddPregnancy.js

--- HealthManagement/component/Mother/AddPregnancy.js
+++ HealthManagement/component/Mother/AddPregnancy.js
@@ -1,5 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, TextInput, Button, StyleSheet ,Alert,ScrollView } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  Alert,
+  ScrollView,
+  TouchableOpacity
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { firebase } from "../../firebase/config";
 
@@ -7,8 +15,25 @@ const AddPregnancy = ({ route }) => {
   const { motherId } = route.params || { motherId: null };
   const navigation = useNavigation();
   const [motherDetails, setMotherDetails] = useState(null);
-  const [babyName, setBabyName] = useState("");
-  const [babyAge, setBabyAge] = useState("");
+
+  const [menstrual, setmenstrual] = useState("");
+  const [delivery, setdelivery] = useState("");
+  const [movement, setmovement] = useState("");
+  const [weeks, setweeks] = useState("");
+
+  const [abortions, setabortions] = useState("");
+  const [Gestational, setGestational] = useState("");
+
+  const [Antepartum, setAntepartum] = useState("");
+  const [Multiple, setMultiple] = useState("");
+  const [Casual, setCasual] = useState("");
+  const [POthers, setPOthers] = useState("");
+
+  const [Diabetes, setDiabetes] = useState("");
+  const [malaria, setmalaria] = useState("");
+  const [Heart, setHeart] = useState("");
+  const [Renal, setRenal] = useState("");
+  const [OOthers, setOOthers] = useState("");
 
   useEffect(() => {
     if (motherDetails) {
@@ -30,7 +55,11 @@ const AddPregnancy = ({ route }) => {
     const fetchMotherDetails = async () => {
       try {
         if (motherId) {
-          const doc = await firebase.firestore().collection("users").doc(motherId).get();
+          const doc = await firebase
+            .firestore()
+            .collection("users")
+            .doc(motherId)
+            .get();
           if (doc.exists) {
             setMotherDetails(doc.data());
           } else {
@@ -45,13 +74,26 @@ const AddPregnancy = ({ route }) => {
     fetchMotherDetails();
   }, [motherId]);
 
-  const handleAddBaby = async () => {
+  const handleAddPregnancy = async () => {
     try {
-      if (motherId && babyName && babyAge) {
-        await firebase.firestore().collection("babies").add({
+      if (motherId) {
+        await firebase.firestore().collection("pregnancy").add({
           motherId: motherId,
-          name: babyName,
-          age: babyAge,
+          menstrual: menstrual,
+          delivery: delivery,
+          movement: movement,
+          weeks: weeks,
+          abortions: abortions,
+          Gestational: Gestational,
+          Antepartum: Antepartum,
+          Multiple: Multiple,
+          Casual: Casual,
+          POthers: POthers,
+          Diabetes: Diabetes,
+          malaria: malaria,
+          Heart: Heart,
+          Renal: Renal,
+          OOthers: OOthers,
         });
 
         // Show an alert after adding the baby details
@@ -62,8 +104,21 @@ const AddPregnancy = ({ route }) => {
             {
               text: "OK",
               onPress: () => {
-                setBabyName(""); // Clearing input fields
-                setBabyAge("");
+                setmenstrual(""); // Clearing input fields
+                setdelivery("");
+                setmovement("");
+                setweeks("");
+                setabortions("");
+                setGestational("");
+                setAntepartum("");
+                setMultiple("");
+                setCasual("");
+                setPOthers("");
+                setDiabetes("");
+                setmalaria("");
+                setHeart("");
+                setRenal("");
+                setOOthers("");
               },
             },
           ],
@@ -76,7 +131,6 @@ const AddPregnancy = ({ route }) => {
       console.error("Error adding baby: ", error);
     }
   };
-
 
   if (!motherDetails) {
     return (
@@ -91,61 +145,196 @@ const AddPregnancy = ({ route }) => {
       <View style={styles.container}>
         <Text style={styles.header}>Obstetric History</Text>
         <View>
-          <Text style={styles.detailText}>01. Date of last menstrual period</Text>
+          <Text style={styles.detailText}>
+            01. Date of last menstrual period
+          </Text>
           <TextInput
-        placeholder="Enter Email"
-        style={styles.textBoxes}
-        // onChangeText={(email) => setEmail(email)}
-        autoCapitalize="none"
-        autoCorrect={false}
-      />
+            placeholder="Enter Email"
+            style={styles.textBoxes}
+            onChangeText={(menstrual) => setmenstrual(menstrual)}
+            autoCapitalize="none"
+            autoCorrect={false}
+          />
         </View>
-        <View >
-          <Text style={styles.detailText}>02. Expected date of birth</Text>
+        <View>
+          <Text style={styles.detailText}>02. Expected period of delivery</Text>
           <TextInput
-        placeholder="Enter Email"
-        style={styles.textBoxes}
-        // onChangeText={(email) => setEmail(email)}
-        autoCapitalize="none"
-        autoCorrect={false}
-      />
+            placeholder="Enter Email"
+            style={styles.textBoxes}
+            onChangeText={(delivery) => setdelivery(delivery)}
+            autoCapitalize="none"
+            autoCorrect={false}
+          />
         </View>
-        <View >
-          <Text style={styles.detailText}>03. The date of the first felt fetal movement</Text>
+        <View>
+          <Text style={styles.detailText}>
+            03. The date of the first felt fetal movement
+          </Text>
           <TextInput
-        placeholder="Enter Email"
-        style={styles.textBoxes}
-        // onChangeText={(email) => setEmail(email)}
-        autoCapitalize="none"
-        autoCorrect={false}
-      />
+            placeholder="Enter Email"
+            style={styles.textBoxes}
+            onChangeText={(movement) => setmovement(movement)}
+            autoCapitalize="none"
+            autoCorrect={false}
+          />
         </View>
-        <View >
-          <Text style={styles.detailText}>04. Number of gestational weeks at registration</Text>
+        <View>
+          <Text style={styles.detailText}>
+            04. Number of gestational weeks at registration
+          </Text>
           <TextInput
-        placeholder="Enter Email"
-        style={styles.textBoxes}
-        // onChangeText={(email) => setEmail(email)}
-        autoCapitalize="none"
-        autoCorrect={false}
-      />
+            placeholder="Enter Email"
+            style={styles.textBoxes}
+            onChangeText={(weeks) => setweeks(weeks)}
+            autoCapitalize="none"
+            autoCorrect={false}
+          />
         </View>
-        <Text style={styles.detailText}>Mother Name: {motherDetails?.name}</Text>
-        <Text style={styles.detailText}>Add Baby:</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Baby's Name"
-          value={babyName}
-          onChangeText={(text) => setBabyName(text)}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Baby's Age"
-          value={babyAge}
-          onChangeText={(text) => setBabyAge(text)}
-          keyboardType="numeric"
-        />
-        <Button title="Add Baby" onPress={handleAddBaby} />
+        <Text style={styles.header}>Risk Conditions</Text>
+          <Text style={styles.header2}>Previous Pregnancies</Text>
+          <View>
+          <Text style={styles.detailText}>
+            01. Previous abortions
+          </Text>
+          <TextInput
+            placeholder="Enter Email"
+            style={styles.textBoxes}
+            onChangeText={(abortions) => setabortions(abortions)}
+            autoCapitalize="none"
+            autoCorrect={false}
+          />
+        </View>
+        <View>
+          <Text style={styles.detailText}>
+            02. Gestational diseases
+          </Text>
+          <TextInput
+            placeholder="Enter Email"
+            style={styles.textBoxes}
+            onChangeText={(Gestational) => setGestational(Gestational)}
+            autoCapitalize="none"
+            autoCorrect={false}
+          />
+        </View>
+        <Text style={styles.header2}>Presnet Pregnancy</Text>
+        <View>
+          <Text style={styles.detailText}>
+            01. Antepartum vaginal bleeding
+          </Text>
+          <TextInput
+            placeholder="Enter Email"
+            style={styles.textBoxes}
+            onChangeText={(Antepartum) => setAntepartum(Antepartum)}
+            autoCapitalize="none"
+            autoCorrect={false}
+          />
+        </View>
+        <View>
+          <Text style={styles.detailText}>
+            02. Multiple pregnancy
+          </Text>
+          <TextInput
+            placeholder="Enter Email"
+            style={styles.textBoxes}
+            onChangeText={(Multiple) => setMultiple(Multiple)}
+            autoCapitalize="none"
+            autoCorrect={false}
+          />
+        </View>
+        <View>
+          <Text style={styles.detailText}>
+            03. Casual position
+          </Text>
+          <TextInput
+            placeholder="Enter Email"
+            style={styles.textBoxes}
+            onChangeText={(Casual) => setCasual(Casual)}
+            autoCapitalize="none"
+            autoCorrect={false}
+          />
+        </View>
+        <View>
+          <Text style={styles.detailText}>
+            04. Others(specify)
+          </Text>
+          <TextInput
+            placeholder="Enter Email"
+            style={styles.textBoxes}
+            onChangeText={(POthers) => setPOthers(POthers)}
+            autoCapitalize="none"
+            autoCorrect={false}
+          />
+        </View>
+        <Text style={styles.header2}>Other Maternal Conditions</Text>
+        <View>
+          <Text style={styles.detailText}>
+            01. Diabetes
+          </Text>
+          <TextInput
+            placeholder="Enter Email"
+            style={styles.textBoxes}
+            onChangeText={(Diabetes) => setDiabetes(Diabetes)}
+            autoCapitalize="none"
+            autoCorrect={false}
+          />
+        </View>
+        <View>
+          <Text style={styles.detailText}>
+            02. malaria
+          </Text>
+          <TextInput
+            placeholder="Enter Email"
+            style={styles.textBoxes}
+            onChangeText={(malaria) => setmalaria(malaria)}
+            autoCapitalize="none"
+            autoCorrect={false}
+          />
+        </View>
+        <View>
+          <Text style={styles.detailText}>
+            03. Heart Disease
+          </Text>
+          <TextInput
+            placeholder="Enter Email"
+            style={styles.textBoxes}
+            onChangeText={(Heart) => setHeart(Heart)}
+            autoCapitalize="none"
+            autoCorrect={false}
+          />
+        </View>
+        <View>
+          <Text style={styles.detailText}>
+            04. Renal Disease
+          </Text>
+          <TextInput
+            placeholder="Enter Email"
+            style={styles.textBoxes}
+            onChangeText={(Renal) => setRenal(Renal)}
+            autoCapitalize="none"
+            autoCorrect={false}
+          />
+        </View>
+        <View>
+          <Text style={styles.detailText}>
+            05. Others(specify)
+          </Text>
+          <TextInput
+            placeholder="Enter Email"
+            style={styles.textBoxes}
+            onChangeText={(OOthers) => setOOthers(OOthers)}
+            autoCapitalize="none"
+            autoCorrect={false}
+          />
+        </View>
+
+        <TouchableOpacity
+        style={styles.buttonStyle}
+        onPress={handleAddPregnancy}
+      >
+        <Text style={styles.buttonText}>Add Pregnancy Details</Text>
+      </TouchableOpacity>
+
+
       </View>
     </ScrollView>
   );
@@ -172,7 +361,13 @@ const styles = StyleSheet.create({
     marginLeft: 30,
   },
   header: {
-    fontSize: 20,
+    fontSize: 22,
+    marginBottom: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  header2: {
+    fontSize: 18,
     marginBottom: 20,
     fontWeight: "bold",
   },
@@ -197,6 +392,23 @@ const styles = StyleSheet.create({
   row: {
     flexDirection: "row",
     justifyContent: "flex-start",
+  },
+  buttonStyle: {
+    backgroundColor: "#FF25A9",
+    padding: 13,
+    borderRadius: 10,
+    width: "100%",
+    height: 50,
+    marginBottom: 20,
+    borderColor: "#FF25A9",
+    borderWidth: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 17,
+    fontWeight: "bold",
   },
 });
 
