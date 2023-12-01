5e5d4ab6070836a4666d83753ecf53ed5641ba7e
Chamaththa Shamod <99489115+Chamaththa123@users.noreply.github.com>
2023-12-01 10:31:52 +0530

    Update Qr.js

--- HealthManagement/component/Mother/Qr.js
+++ HealthManagement/component/Mother/Qr.js
@@ -1,9 +1,11 @@
-import { View, Text } from 'react-native'
 import React, { useState, useEffect } from "react";
+import { View, Text, ScrollView, StyleSheet } from "react-native";
 import { firebase } from "../../firebase/config";
+import QRCode from "react-native-qrcode-svg";
 
-const Qr = () => {
-    const [name, setName] = useState({});
+const Qr = ({ navigation }) => {
+  const [name, setName] = useState({});
+  const [qrData, setQrData] = useState("");
 
   useEffect(() => {
     const userUid = firebase.auth().currentUser.uid;
@@ -12,16 +14,48 @@ const Qr = () => {
     userDocRef.get().then((snapshot) => {
       if (snapshot.exists) {
         setName(snapshot.data());
+        setQrData(snapshot.data().register_No);
       } else {
         console.log("User doesn't exist");
       }
     });
   }, []);
-  return (
-    <View>
-      <Text>{name.register_No}</Text>
-    </View>
-  )
-}
 
-export default Qr
+  useEffect(() => {
+    navigation.setOptions({
+      headerTitleStyle: {
+        fontWeight: "bold",
+        color: "#57ADF8",
+        fontSize: 20,
+      },
+      headerTintColor: "#57ADF8",
+      headerShown: true,
+      title: "My QR Code",
+      headerTitleAlign: "center",
+    });
+  }, [navigation]);
+
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <View style={styles.content}>
+        {qrData ? <QRCode value={qrData} size={300} /> : null}
+      </View>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  content: {
+    justifyContent: "center",
+    alignItems: "center",
+    paddingVertical: 20,
+  },
+});
+
+export default Qr;
