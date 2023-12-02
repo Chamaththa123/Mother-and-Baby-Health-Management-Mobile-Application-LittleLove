88cb57f63a54723c20369a8ab19a9beaf2461b91
Chamaththa Shamod <99489115+Chamaththa123@users.noreply.github.com>
2023-12-02 16:05:40 +0530

    Create Loading.js

---
+++ HealthManagement/component/Loading.js
@@ -0,0 +1,64 @@
+import React, { useEffect } from "react";
+import { View, Image, Text, TouchableOpacity, StyleSheet } from "react-native";
+
+import MyImage from "../assets/logo.png";
+
+const Start = ({ route, navigation }) => {
+  const handleLoginPress = () => {
+    navigation.navigate("Login");
+  };
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
+  return (
+    <View>
+      <Image source={MyImage} style={styles.image} />
+      <View style={styles.textContainer}>
+        <Text style={styles.redText}>Little </Text>
+        <Text style={styles.blueText}> Love</Text>
+      </View>
+      <Text style={styles.text}>Loading</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+  image: {
+    width: "30%",
+    height: "16%",
+  },
+  textContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: "1%",
+  },
+  redText: {
+    color: "#57ADF8",
+    fontSize: 20,
+    fontWeight: "600",
+    textAlign: "center",
+  },
+  blueText: {
+    color: "#FF25A9",
+    fontSize: 20,
+    fontWeight: "600",
+    textAlign: "center",
+  },
+  text:{
+    fontSize:17,
+    color:'#FF25A9',
+    margin:30
+  }
+});
+
+export default Start;
