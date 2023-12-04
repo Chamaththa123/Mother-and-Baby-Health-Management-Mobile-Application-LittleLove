11becd2422b6c14a23efe6c8dab20cc83925961c
Chamaththa Shamod <99489115+Chamaththa123@users.noreply.github.com>
2023-12-04 23:30:47 +0530

    Update Clinic.js

--- HealthManagement/component/Mother/Pregnancy/Clinic/Clinic.js
+++ HealthManagement/component/Mother/Pregnancy/Clinic/Clinic.js
@@ -1,26 +1,55 @@
-import React, { useState } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
-import { TabView, TabBar } from 'react-native-tab-view';
+import React, { useState, useEffect } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import { TabView, TabBar } from "react-native-tab-view";
+import { useNavigation } from "@react-navigation/native";
 
 const Clinic = () => {
+  const navigation = useNavigation();
   const [index, setIndex] = useState(0);
   const [routes] = useState([
-    { key: 'clinic', title: 'Clinic' },
-    { key: 'vaccine', title: 'Vaccine' },
+    { key: "vaccine", title: "Vaccine Information" },
+    { key: "clinic", title: "Clinic Information" },
+    { key: "health", title: "Health Graphs" },
   ]);
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerTitleStyle: {
+        fontWeight: "bold",
+        color: "#57ADF8",
+        fontSize: 20,
+      },
+      headerTintColor: "#57ADF8",
+      headerShown: true,
+      title: "Care At Clinic",
+      headerTitleAlign: "center",
+    });
+  }, [navigation]);
 
   const renderScene = ({ route }) => {
     switch (route.key) {
-      case 'clinic':
+      case "clinic":
         return (
-          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <View
+            style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+          >
             <Text>Clinic Content</Text>
           </View>
         );
-      case 'vaccine':
+      case "vaccine":
         return (
-          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <View
+            style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+          >
             <Text>Vaccine Content</Text>
+          </View>
+        );
+      case "health":
+        return (
+          <View
+            style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+          >
+            <Text>health Content</Text>
           </View>
         );
       default:
@@ -28,17 +57,19 @@ const Clinic = () => {
     }
   };
 
-  const renderTabBar = props => (
+  const renderTabBar = (props) => (
     <TabBar
       {...props}
-      indicatorStyle={{ backgroundColor: 'blue' }}
-      style={{ backgroundColor: 'white' }}
+      indicatorStyle={{ backgroundColor: "blue" }}
+      style={{ backgroundColor: "white" }}
       renderLabel={({ route, focused, color }) => (
         <TouchableOpacity
-          style={{ flex: 1, alignItems: 'center' }}
-          onPress={() => setIndex(route.key === 'clinic' ? 0 : 1)}
+          style={{ flex: 1, alignItems: "center" }}
+          onPress={() => setIndex(route.key === "clinic" ? 0 : 1)}
         >
-          <Text style={{ color: focused ? 'blue' : 'black' }}>{route.title}</Text>
+          <Text style={{ color: focused ? "blue" : "black" }}>
+            {route.title}
+          </Text>
         </TouchableOpacity>
       )}
     />
