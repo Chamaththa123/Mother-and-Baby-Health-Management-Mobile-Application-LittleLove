a1a72f3b04b07ff5b97e5f58510efcc880e6199c
Chamaththa Shamod <99489115+Chamaththa123@users.noreply.github.com>
2023-12-04 10:30:29 +0530

    Create BabyList.js

---
+++ HealthManagement/component/Mother/Baby/BabyList.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const BabyList = () => {
+  return (
+    <View>
+      <Text>BabyList</Text>
+    </View>
+  )
+}
+
+export default BabyList
